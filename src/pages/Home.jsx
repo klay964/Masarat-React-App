17d841dceeef92f5779faff01a551eb553d4e67f
Mohammed Ali <mohammed.a.hasan20@gmail.com>
2023-03-31 23:36:29 +0300

    add state to store input values

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,37 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Home() {
-  const users = [
-    {
-      name: 'ahmed',
-      age: 20,
-    },
-    {
-      name: 'mohammed',
-      age: 30,
-    },
-    {
-      name: 'fatima',
-      age: 22,
-    },
-    {
-      name: 'sara',
-      age: 50,
-    },
-  ];
+  const [name, setName] = useState(localStorage.getItem('name'));
+  const [age, setAge] = useState(localStorage.getItem('age'));
+
+  const handleName = (event) => {
+    setName(event.target.value);
+  };
+  const handleAge = (e) => {
+    setAge(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    localStorage.setItem('name', name);
+    localStorage.setItem('age', age);
+
+    console.log(name);
+    console.log(age);
+  };
   return (
     <main style={{ backgroundColor: 'yellow' }}>
-      <h1>Home Page</h1>
-      <h2>Users</h2>
-      <div>
-        {users.map((user) => (
-          <>
-            <p>{user.name}</p>
+      <h1>Home</h1>
+      <form onSubmit={handleSubmit}>
+        <div>name:{name}</div>
+        <div>age:{age}</div>
 
-            <p>{user.age}</p>
-          </>
-        ))}
-      </div>
+        <input type='text' name='name' onChange={handleName} />
+        <input type='number' name='age' onChange={handleAge} />
+        <button type='submit'>Submit</button>
+      </form>
+      <button>Click me</button>
     </main>
   );
 }
