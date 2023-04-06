92cf9f31690787b227b2f99ae5f04d53778a6b0d
Mohammed Ali <mohammed.a.hasan20@gmail.com>
2023-04-06 17:30:19 +0300

    fetch data using axios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,31 @@
-import React, { useState } from 'react';
-import Date from '../components/Date';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function Home() {
-  const [title, setTitle] = useState('Melad');
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users').then((response) => {
+      setUsers(response.data);
+    });
+  }, []);
+
+  const deleteUser = (id) => {
+    axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
+  };
+
+  console.log(users);
 
   return (
-    <main style={{ backgroundColor: 'yellow' }}>
+    <main style={{ backgroundColor: 'white' }}>
       <h1>Home</h1>
-      <Date person={title} name='Mohammed' age={20} changeState={setTitle} />
-      <div></div>
+      <div>users</div>
+      {users &&
+        users.map((user, i) => (
+          <div key={user.id}>
+            <p>{user.name}</p>
+            <button onClick={() => deleteUser(user.id)}>delete</button>
+          </div>
+        ))}
     </main>
   );
 }
