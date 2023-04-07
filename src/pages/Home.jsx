d3b9ec214f947c150266893ae5bf779a0246bde3
Mohammed Ali <mohammed.a.hasan20@gmail.com>
2023-04-07 13:58:07 +0300

    add create post

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,103 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Loading from '../components/Loading';
 
 function Home() {
-  const [users, setUsers] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
+  const fetchData = () => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/posts?_page=1&_limit=20')
+      .then((response) => {
+        console.log(response);
+        if (response && response.status === 200) {
+          setPosts(response.data);
+          setLoading(false);
+          setError(false);
+        } else {
+          setError('Error Found');
+        }
+      })
+      .catch((err) => {
+        setError('page not found 404');
+      });
+  };
   useEffect(() => {
-    axios.get('https://jsonplaceholder.typicode.com/users').then((response) => {
-      setUsers(response.data);
-    });
+    fetchData();
   }, []);
 
-  const deleteUser = (id) => {
-    axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
+  const deletePost = (id) => {
+    axios.delete(`https://jsonplaceholder.typicode.com/posts/${id}`);
+    fetchData();
   };
 
-  console.log(users);
+  const handleTitle = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleBody = (e) => {
+    setBody(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post('https://jsonplaceholder.typicode.com/posts', {
+        title,
+        body,
+      })
+      .then((response) => {
+        fetchData();
+      });
+  };
 
   return (
     <main style={{ backgroundColor: 'white' }}>
       <h1>Home</h1>
       <div>users</div>
-      {users &&
-        users.map((user, i) => (
-          <div key={user.id}>
-            <p>{user.name}</p>
-            <button onClick={() => deleteUser(user.id)}>delete</button>
-          </div>
-        ))}
+      {error ? (
+        <>{error}</>
+      ) : (
+        <div>
+          {loading ? (
+            <Loading />
+          ) : (
+            <div>
+              <div>
+                <h1>Add Post</h1>
+                <form onSubmit={handleSubmit}>
+                  <input
+                    type='text'
+                    value={title}
+                    name='title'
+                    onChange={handleTitle}
+                    placeholder='Name'
+                  />
+                  <input
+                    type='text'
+                    value={body}
+                    onChange={handleBody}
+                    name='body'
+                    placeholder='Body'
+                  />
+                  <button type='submit'>Submit</button>
+                </form>
+              </div>
+              {posts &&
+                posts.map((post, i) => (
+                  <div key={post.id}>
+                    <p>{post.title}</p>
+                    <button onClick={() => deletePost(post.id)}>delete</button>
+                  </div>
+                ))}
+            </div>
+          )}
+        </div>
+      )}
     </main>
   );
 }
